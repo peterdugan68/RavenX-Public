3eadce9c4d9c4c74ca0f73c3f3a9131348be7e90
Peter Dugan <peterdugan68@gmail.com>
2019-09-02 23:12:25 -0400

    GOM-PAM integration of EARS codec

--- config/+ears/earsread2018.m
+++ config/+ears/earsread2018.m
@@ -1,54 +1,51 @@
 function [pcm] = earsread2018(fname, blk)
-% 
+%
 %   Usage:
 %       [pcm] = earsread(fname, blk)
 %
 %   input
 %       fname - EARS file
 %         blk - read block
-% 
+%
 %   output:
 %       pcm - pcm data, range [-1..1]
-% 
+%
 % Copyright 2013-2019 Cornell Lab of Ornithology
 %   PDugan, based on memmap FileSignal JZollweg and EARS sensor info
 
 intsize = 16;
-[~, ~, endian] = computer;
 
-if strcmp(endian, 'L')
-    machineformat = 'ieee-be';
-else
-    machineformat = 'ieee-le';
-end
-
-
-fd = fopen(fname{1}, 'r',machineformat );
-if (fd < 0)
-    fprintf('File not present: %s\n', filename);
-    return
-end
-fseek(fd,12,'bof');
-pcm = fread(fd, [250, Inf], '250*int16', 12, machineformat);  % read all data blocks, skip headers
-pcm = reshape(pcm, [numel(pcm),1]);
-
+[~,~,endian] = computer;
 
 try
     if nargin == 2
-        pcm = pcm(blk(1):blk(2),1);  %return the block requested
+        numFrames = blk;  %return the block requested
+    else
+        numFrames = Inf;
     end
+    
+    % read raw uint16 data
+    [pcmUINT16] = ears.earsreadUINT16(fname,numFrames);
+
+    if strcmp(endian,'L')
+        pcm = bitshift(pcmUINT16,8) + bitshift(pcmUINT16, -8);
+    end
+    
+    
 catch ME
     if strcmp(ME.identifier, 'MATLAB:badsubscript')
         pcm = [];  % cannot gaurentee the data
         warning('earsread2018 %s', ME.identifier);
-    else 
+    else
         rethrow(ME);
     end
 end
 
-normfac = 2^(1 - intsize);
+normfac = 1/1000;
+pcm = typecast(pcm(:), 'int16');
+pcm = reshape(pcm, [1, numel(pcm)]);
+
 pcm = bsxfun(@times, double(pcm), normfac);   % scale result
 
-fclose(fd);
 end
 
