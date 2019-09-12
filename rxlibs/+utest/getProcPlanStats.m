1583895973d3f4a40097dff8d42452d7e4f66e1f
Peter Dugan <peterdugan68@gmail.com>
2019-09-11 22:29:38 -0400

    updates to handle corrupt files.

--- rxlibs/+utest/getProcPlanStats.m
+++ rxlibs/+utest/getProcPlanStats.m
@@ -1,6 +1,13 @@
 function [outputArg1,outputArg2] = getProcPlanStats(inputArg1,inputArg2)
 %
 %
+% 
+% 
+% 
+% 
+% 
+% 
+%  pdugan 2019 
 
 ChanMode = HpcAppManage.ChanMode;
 
@@ -17,8 +24,13 @@ sndPath = uigetdir(projPath, 'Pick a directory that contains sound files');
 % Step-B
 % specify output folder, this will be the actual hours recorded from the
 % run.
-dataPath = '\\hpcnas\rx_store\project_data\ravenx_data\Proj_2017_HDR_GoMex_80670\Proj_80670GOMEX02\output\PSDhann_1s1hz';
-
+dataPath = '\\hpcnas\rx_store\project_data\ravenx_data\Proj_2017_HDR_GoMex_80670\Proj_80670GOMEX02\output';
+algName = 'PSDhann_1s1hz';
+dataPath = fullfile(dataPath, algName);
+d = dir(dataPath);
+fInd = find([d(1:end).isdir]);
+d = d(fInd(3:end));
+dTble = struct2table(d);
 
 % create difference stats by comapring A vs B
 
@@ -47,27 +59,59 @@ ChanMode = ChanMode.findSoundsAndChanMode(sndPath, input_path);
 SndObjTble = ChanMode.JobSndTbl;
 
 % construct sound plan with array package object.
-SoundPlanTblObj = HpcAppManage.SoundPlan(ChanMode.ArrayPkgObj);
+inSoundPlanTblObj = HpcAppManage.SoundPlan(ChanMode.ArrayPkgObj);
 
 % open existing table, create if does not exist
-SoundPlanTblObj = SoundPlanTblObj.OpenCreateSoundPlan(RxProjAppObj.plan_path, RxProjAppObj.preset_name);
+inSoundPlanTblObj = inSoundPlanTblObj.OpenCreateSoundPlan(RxProjAppObj.plan_path, RxProjAppObj.preset_name);
+
+SoundPlanTbl = inSoundPlanTblObj.SoundPlanTbl;
 
 % build the sound plan table and save each plan entry
 for h = 1:size(SndObjTble.SoundSet,1);
     
-    SoundPlanTblObj.SoundSet = SndObjTble.SoundSet(h);
-    SoundPlanTblObj.ArrayPkgObj = SndObjTble.SnsrTbl{h};
+    inSoundPlanTblObj.SoundSet = SndObjTble.SoundSet(h)
+    inSoundPlanTblObj.ArrayPkgObj = SndObjTble.SnsrTbl{h};
     
     % get sounds from plan
-    [SoundPlanTblObj, err] = SoundPlanTblObj.GetSoundList(SndObjTble.soundFiles(h));
+    [inSoundPlanTblObj, err] = inSoundPlanTblObj.GetSoundList(SndObjTble.soundFiles(h));
     
     if ~isempty(err)
         return;
     end
     
+    % build the hour 
+    bSigs = blockByHour(inSoundPlanTblObj.soundFiles);
+    days = unique([bSigs.day]); % discover number of days to process
+    bsTble = struct2table(bSigs);
+    
+    [~, nme, ~] = fileparts(inSoundPlanTblObj.SoundSet{1});
+    fInd = find(contains(dTble.name, nme));
+    oPath = fullfile(dataPath,dTble.name{fInd}); 
+    od = dir(fullfile(oPath, '*.mat'));
+    I = 1:length(od);
+    [dy, hr] = arrayfun(@(x) (parseDateHour(od(x).name)), I', 'UniformOutput', false);
+    eT = table;
+    eT.outfile = {od.name}';
+    eT.alg = repmat({algName}, size(eT.outfile,1) ,1);
+    eT.day = [dy{:}]';    
+    eT.hour = [hr{:}]';
+    
+
+    SoundPlanTbl.CompStatus{fInd} = outerjoin(eT,bsTble,'Mergekeys', true);
+    nInd = find(strcmp(SoundPlanTbl.CompStatus{fInd}.outfile, ''));
+    SoundPlanTbl.skipHr{fInd} = SoundPlanTbl.CompStatus{fInd}(nInd,{'day', 'hour', 'hrSigs', 'startFrame', 'endFrame'});
+end
+
+% save the Sound Plan to re-run
+% SoundPlanTbl = outSoundPlanTblObj.SoundPlanTbl;
+save(fullfile(inSoundPlanTblObj.planPath,[inSoundPlanTblObj.planFileName '_reRun.mat']), 'SoundPlanTbl');
+
 end
 
 
+function [dy, hr] = parseDateHour(c)
+
+dy = datenum(c(1:8), 'yyyymmdd');
+hr = str2num(c(10:11));
 
 end
-
